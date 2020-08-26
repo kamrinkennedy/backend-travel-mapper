3da7324ea5a1fd6f6443aff15b40d77a3ad3a43a
Kamrin <kamrinkennedy@gmail.com>
2020-08-25 20:13:56 -0500

    add destinations

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -4,5 +4,19 @@ class DestinationsController < ApplicationController
         destinations = Destination.all
         render json: destinations.to_json(include: {activities: {only: [:name, :description, :cost]}}, except: [:updated_at, :created_at])
     end
+
+    def create
+        # byebug
+        destination = Destination.new(destination_params)
+        if destination.save
+            render json: destination
+        else
+            render json: {error: 'Could not be created'}
+        end
+    end
+
+    def destination_params
+        params.require(:destination).permit(:location, :locale, :arrival, :departure)
+    end
     
 end
