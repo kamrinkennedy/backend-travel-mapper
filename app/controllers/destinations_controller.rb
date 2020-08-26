8f98a43e3390bc0b3f34caaaa52fcbc96a2ef2fa
Kamrin <kamrinkennedy@gmail.com>
2020-08-26 18:01:03 -0500

    destination crud

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -15,8 +15,25 @@ class DestinationsController < ApplicationController
         end
     end
 
+    def update
+        destination = Destination.find(params[:id])
+        if destination.update(destination_params)
+            render json: destination
+        else
+            render json: {error: 'Could not update'}
+        end
+    end
+    
+    def destroy
+        destination = Destination.find(params[:id])
+        destination.destroy
+        
+        render json: {message: "Successfully deleted #{destination.location} trip."}
+    end
+    
+    private
+    
     def destination_params
         params.require(:destination).permit(:location, :locale, :arrival, :departure)
     end
-    
 end
